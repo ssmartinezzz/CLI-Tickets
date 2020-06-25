c2b53e7636012eac78d982cc3b3b968ede3881eb
s.martinez <santiskatersan@gmail.com>
2020-06-25 11:20:42 -0300

    Documenting client_functions.py

--- client_functions.py
+++ client_functions.py
@@ -1,16 +1,22 @@
 import sys
-
 import cliController
 import multiprocessing
 import messages
 from jsonService import *
 import time
-
 from utils import *
 
-
-def exportTickets(socket,filtersapplied,ticketData):
-
+def exportTickets(socket, filtersapplied, ticketData):
+    """
+    function executed in a paralell process with @client_ticketExport
+    that search Tickets depending on the filters applied and then export
+    them to a csv compressed file (gz)
+    @param socket: (Socket object where the connection between Server - Client is established
+    and can send or receive data through it)
+    @param filtersapplied: (list of filters inserted in the command line
+    by the client. Any - author - description - date)
+    @param ticketData:(Values that are going to be applied to search using filters)
+    """
     socket.send(filtersapplied.encode())
 
     socket.send(ticketData.encode())
@@ -26,6 +32,13 @@ def exportTickets(socket,filtersapplied,ticketData):
     print(messages.CLIENT_EXPORT_SUCCESS)
 
 def client_ticketInsertion(client,destination):
+    """
+    function that allows a client to send the specific values required
+    to insert a new Ticket object in the database.
+    @param client: (Is the socket connection where the data is sent or recieved)
+    @param destination: The main action that is executed, e.g List | Insert | export).
+    This value is sent to the Server.
+    """
     clearTerminal()
 
     client.send(destination.encode())
@@ -41,7 +54,12 @@ def client_ticketInsertion(client,destination):
     print(client.recv(1024).decode())
 
 def client_ticketList(client, destination):
-
+    """
+    function that allows a client to send the
+    different values needed to list an existing Ticket
+    @param client:(Is the socket connection where the data is sent or recieved)
+    @param destination:(The main action that is executed, e.g List | Insert | export)
+    """
     client.send(destination.encode())
 
     clearTerminal()
@@ -64,10 +82,18 @@ def client_ticketList(client, destination):
 
     list_tickets = eval(ticket_search)
 
-    printableTicket(list_tickets)
+    print(list_tickets)
 
-def client_ticketEdition(client,destination):
-
+def client_ticketEdition(client, destination):
+    """
+    function that enable a client to edit a ticket
+    by giving an ID value and the modifications of it.
+    This function verifies if the given ID format is correct by using
+    @idValidator and it verifies through the server if the correct format
+    given id exist on the DB.
+    @param client: (Socket Object for communication)
+    @param destination: (value of the current action that is being executed)
+    """
     clearTerminal()
 
     client.send(destination.encode())
@@ -104,7 +130,12 @@ def client_ticketEdition(client,destination):
         sys.exit()
 
 def client_ticketExport(client, destination):
-
+    """
+    Main function used for ticket exportation. It launches a parallel process
+    using multiprocessing.Process().The parallel process recieves tickets and put them in a compressed CSV file
+    @param client: (Socket object for connections and communications)
+    @param destination: (Value of the specific task that is being executed)
+    """
     client.send(destination.encode())
 
     clearTerminal()
@@ -126,12 +157,23 @@ def client_ticketExport(client, destination):
     paralell_p.join()
 
 def client_clearTerminal(client, destination):
-
+    """
+    function that clears clients's terminals.
+    @param client: (Socket object for communication)
+    @param destination: (Value of the current action that is sent to the server through the socket object)
+    """
     clearTerminal()
 
     client.send(destination.encode())
 
 def client_exit(client, destination):
+    """
+    function that disconnects the client from the server.
+    After sending the destination value, the connection will be closed.
+    @param client: (Socket object)
+    @param destination: (Value of the operation executed, is sent to the server through the socket object)
+
+    """
     clearTerminal()
 
     client.send(destination.encode())
